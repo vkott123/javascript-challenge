2b77e9cd6fd126c24c4783b0963c02f0db240a36
vkott123 <40189816+vkott123@users.noreply.github.com>
2019-08-29 20:09:19 -0400

    Level 1 complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function appendRowsAndData(obj) {
+    var row = tbody.append("tr");
+
+    // Below loop assumes object keys are in same order and are present every time
+    Object.entries(obj).forEach(([key, value]) => {
+        row.append("td").text(value); 
+    })
+};
+
+// Append all table rows and data
+data.forEach(appendRowsAndData);
+
+var button = d3.select("#filter-btn");
+
+// Only runs when button is clicked or user presses enter
+button.on("click", function() {
+    d3.event.preventDefault();
+
+    var dateInput = d3.select("#datetime");
+    var datetime = dateInput.property("value");
+
+    var filterInputs = {};
+
+    if (datetime !== "") {
+        filterInputs.datetime = datetime;
+    }
+
+    var filtered = tableData.filter(obj => {
+        var criteria = true;
+        Object.entries(filterInputs).forEach(([key, value]) => {
+            criteria = criteria && (obj[key] === value);
+        });
+        return criteria;
+    });
+
+    console.log(filtered);
+
+    tbody.html("");
+
+    filtered.forEach(appendRowsAndData);
+});
